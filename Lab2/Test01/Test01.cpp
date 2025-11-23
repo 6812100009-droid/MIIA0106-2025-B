3f5dc3f76f44789ef3fd58781a8f981ee8c92277
Thanat Kwanmuang MUT <6812100009>
2025-11-23 17:11:14 +0700

    การทดลองครั้งที่ 2

---
+++ Lab2/Test01/Test01.cpp
@@ -0,0 +1,15 @@
+// Test01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "HelloLab2!\n";
+
+    // เพิ่มข้อมูลส่วนตัว
+    std::cout << "Name: ThanaT Kwanmuang MUT\n";
+    std::cout << "Course: MIIA0106-2025-B\n";
+    std::cout << "Email: 6812100009@mut.ac.th\n";
+}
