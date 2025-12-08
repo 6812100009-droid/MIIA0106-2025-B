053877f5d355ca8cf7b252a0b33c61b1ffa340ad
ThanatMUT <6812100009@mut.ac.th>
2025-12-08 16:31:02 +0700

    Lab3

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void main()
+{
+	string itemName;
+	float price;
+	int qty;
+	float total;
+
+	cout << "Enter Item Name: ";
+	cin >> itemName;
+
+	cout << "Enter Price: ";
+	cin >> price;
+
+	cout << "Enter Quantity: ";
+	cin >> qty;
+
+	total = price * qty;
+
+	cout << "|||||||Invoidce|||||||" << endl;
+	cout << "Item Name: " << itemName << endl;
+	cout << "Price: " << price << endl;
+	cout << "Quantity: " << qty << "Pcs" << endl;
+	cout << "Total: " << total << endl;
+	cout << "||||||||||||||||||||||" << endl;
+
+}
